0c916820ca85c0ac77bbd79de2e7ff50286dbd7e
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2023-12-22 11:51:12 +0530

    Make predictions on raw audio and npz

--- tf/tflite_tester.py
+++ tf/tflite_tester.py
@@ -1,10 +1,12 @@
 import csv
 import os
 import sys
+import tracemalloc
+import datetime
 
-import librosa
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
+from pydub import AudioSegment
 
 
 def padding(sound, size):
@@ -16,6 +18,73 @@ def multi_crop(sound, audio_length, n_crops):
     stride = (len(sound) - audio_length) // (n_crops - 1)
     sounds = [sound[stride * i: stride * i + audio_length] for i in range(n_crops)]
     return np.array(sounds)
+
+
+def inference():
+    # Load TFLite model and allocate tensors.
+    interpreter = tflite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Test model on random input data.
+    input_shape = input_details[0]['shape']
+    input_length = input_shape[2]
+    sr = 20000
+    crops = 3
+
+    y_pred = []
+    y = []
+
+    for raw_audio_file in raw_audio_files:
+        filename = os.path.join(data, raw_audio_file)
+        label = raw_audio_file.split('-')[-1].split('.')[0]
+
+        # Read the WAV file using pydub and set the frame rate to 20,000 Hz
+        audio = AudioSegment.from_file(filename, format="wav").set_frame_rate(sr).set_channels(1)
+
+        # Get raw PCM data as a byte string
+        raw_data = audio.raw_data
+
+        # Convert the byte string to a NumPy array of 16-bit integers
+        audio_frames = np.frombuffer(raw_data, dtype=np.int16)
+
+        # Convert the integer audio data to float in the range [-1, 1]
+        normalised_audio = audio_frames.astype(np.float32) / 32767.0
+
+        if len(normalised_audio) > original_input_length:
+            continue
+
+        padded_audio = padding(normalised_audio, original_input_length)
+        cropped_audios = multi_crop(padded_audio, input_length, crops)
+
+        for audio in cropped_audios:
+            np_audio = np.array(audio, dtype=np.float32).reshape((1, 1, input_length, 1))
+
+            # start measuring inference time
+            st = datetime.datetime.now()
+
+            interpreter.set_tensor(input_details[0]['index'], np_audio)
+
+            interpreter.invoke()
+
+            # The function `get_tensor()` returns a copy of the tensor data.
+            # Use `tensor()` in order to get a pointer to the tensor.
+            output_data = interpreter.get_tensor(output_details[0]['index'])
+            pred = output_data.argmax() + 1
+
+            # end measuring inference time
+            et = datetime.datetime.now()
+            infer_time = (et - st).total_seconds() * 1000
+
+            infer_times.append(infer_time)
+
+            y.append(label)
+            y_pred.append(pred)
+
+    return y_pred, y
 
 
 model_path = None
@@ -33,43 +102,21 @@ data = os.path.join(os.getcwd(), 'datasets/fsc22/tflite_test_data')
 raw_audio_files = [f for f in os.listdir(data)]
 original_input_length = 100000
 
-# Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
+infer_times = []
 
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+tracemalloc.start()
 
-# Test model on random input data.
-input_shape = input_details[0]['shape']
-input_length = input_shape[2]
-crops = 3
+predictions, labels = inference()
 
-predictions = []
-labels = []
+current, peak = tracemalloc.get_traced_memory()
 
-for raw_audio_file in raw_audio_files:
-    filename = os.path.join(data, raw_audio_file)
-    label = raw_audio_file.split('-')[-1].split('.')[0]
+tracemalloc.stop()
 
-    normalised_audio, sample_rate = librosa.load(filename, sr=20000, mono=True)
+print('Current memory [MB]: {}, Peak memory [MB]: {}'.format(round(current / (1024 * 1024), 4),
+                                                             round(peak / (1024 * 1024), 4)))
 
-    padded_audio = padding(normalised_audio, original_input_length)
-    cropped_audios = multi_crop(padded_audio, input_length, crops)
-
-    for audio in cropped_audios:
-        np_audio = np.array(audio, dtype=np.float32).reshape((1, 1, input_length, 1))
-        interpreter.set_tensor(input_details[0]['index'], np_audio)
-
-        interpreter.invoke()
-
-        # The function `get_tensor()` returns a copy of the tensor data.
-        # Use `tensor()` in order to get a pointer to the tensor.
-        output_data = interpreter.get_tensor(output_details[0]['index'])
-        pred = output_data.argmax() + 1
-        labels.append(label)
-        predictions.append(pred)
+print(f"Infer time for {len(infer_times)} samples: {np.sum(infer_times)} ms")
+print(f"Infer time per sample: {np.mean(infer_times)} ms")
 
 # Combine arrays into a list of rows
 rows = list(zip(labels, predictions))
---
+++ tf/tflite_tester_npz.py
@@ -0,0 +1,96 @@
+import math
+import os
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+
+class Tester:
+    def __init__(self):
+        self.testX = None
+        self.testY = None
+
+    # Loading Test data
+    def load_data(self):
+        data = np.load(os.path.join(os.getcwd(), 'datasets/fsc22/test_data_20khz/fold1_test3900.npz'),
+                       allow_pickle=True)
+        self.testX = data['x']
+        self.testY = data['y']
+        print(self.testX.shape)
+        print(self.testY.shape)
+
+    # Test the model with test data
+    def validate(self, input_config, model, output_config):
+        y_pred = None
+        y_target = self.testY
+        batch_size = (batchSize // nCrops) * nCrops
+
+        for idx in range(math.ceil(len(self.testX) / batch_size)):
+            x = self.testX[idx * batch_size: (idx + 1) * batch_size]
+            scores = None
+
+            for audio in x:
+                audio = audio.reshape((1, 1, inputLength, 1))
+                model.set_tensor(input_config[0]['index'], audio)
+                model.invoke()
+                output_data = model.get_tensor(output_config[0]['index'])
+                scores = output_data if scores is None else np.concatenate((scores, output_data))
+
+            y_pred = scores if y_pred is None else np.concatenate((y_pred, scores))
+
+        acc = self.compute_accuracy(y_pred, y_target)
+        return acc
+
+    # Calculating average prediction (10 crops) and final accuracy
+    def compute_accuracy(self, y_pred, y_target):
+        # Reshape y_pred to shape it like each sample contains 10 samples.
+        y_pred = y_pred.reshape(y_pred.shape[0] // nCrops, nCrops, y_pred.shape[1])
+        y_target = y_target.reshape(y_target.shape[0] // nCrops, nCrops, y_target.shape[1])
+
+        # Calculate the average of class predictions for 10 crops of a sample
+        y_pred = np.mean(y_pred, axis=1)
+        y_target = np.mean(y_target, axis=1)
+
+        # Get the indices that have the highest average value for each sample
+        y_pred = y_pred.argmax(axis=1)
+        y_target = y_target.argmax(axis=1)
+        accuracy = (y_pred == y_target).mean() * 100
+
+        return accuracy
+
+    # Load and test the saved model
+    def TestModel(self, model_file):
+        interpreter = tf.lite.Interpreter(model_path=model_file)
+        interpreter.allocate_tensors()
+        print('Model Loaded.')
+
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        self.load_data()
+        print('Test dataset loaded.')
+        val_acc = self.validate(input_details, interpreter, output_details)
+        print('Testing - Acc(top1) {:.2f}%'.format(val_acc))
+
+
+if __name__ == '__main__':
+    sr = 20000
+    inputLength = 30225
+    nCrops = 10
+    batchSize = 64
+
+    model_path = None
+
+    while model_path is None:
+        if len(sys.argv) == 2:
+            model_path = sys.argv[1]
+        else:
+            print('Enter model name as command line argument')
+            exit()
+
+    print('Model has been found at: {}'.format(model_path))
+
+    tester = Tester()
+    tester.TestModel(model_path)
